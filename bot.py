3b88654a68af4d38b725e5a40c50a9e4d5b35740
LiquidRekt <dung6ctd@gmail.com>
2020-04-24 16:30:23 +0700

    change

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ async def on_message(message):
     else:
         if "uwu" in message.content.lower():
             await message.channel.send("UwU")
-        if "!deck" in message.content.lower():
+        elif "!deck" in message.content.lower():
             await message.channel.send("Hiện tại chức năng tra Deck không khả dụng. Tui sẽ đem đến cho mấy bro sớm nhất có thể! OwO")
-        if (message.content.lower()[0] == "!"):
+        elif (message.content.lower()[0] == "!"):
             await message.channel.send(message.author + " AWAITING FUNCTION")
 
 client.run(os.environ['DISCORD_TOKEN'])
