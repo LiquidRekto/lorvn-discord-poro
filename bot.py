1110e3a5e0fedc34649c09bda9d1c349f12ea48a
LiquidRekt <dung6ctd@gmail.com>
2020-04-24 15:06:53 +0700

    Fix bot

--- bot.py
+++ bot.py
@@ -10,9 +10,7 @@ async def on_message(message):
     if message.author.bot:
         return
     else:
-        if "UwU" in message.content.lower():
+        if "uwu" in message.content.lower():
             await message.channel.send("UwU")
-        else:
-            await message.channel.send("Wazzup {message.author}!")
 
 client.run(os.environ['DISCORD_TOKEN'])
