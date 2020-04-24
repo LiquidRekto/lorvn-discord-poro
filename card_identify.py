c57d6dae0fe4d0c845c6cf25f79e0d2d17c0cc90
LiquidRekt <dung6ctd@gmail.com>
2020-04-24 20:07:30 +0700

    update url source

--- card_identify.py
+++ card_identify.py
@@ -1,6 +1,6 @@
 import requests
 import json
-response = json.loads(requests.get("your_url").text)
+response = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/en_us/data/set1-en_us.json").text)
 
 def retrieveCardsData():
     for card in response:
