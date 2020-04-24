8a64e91e8f2ef33d99cf5fe496bb49629d526361
LiquidRekt <dung6ctd@gmail.com>
2020-04-24 13:43:20 +0700

    Add bot functions

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ from twisted_fate import Deck
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print("Bot is ready!")
+async def on_message(message):
+    if message.author.bot:
+        return
+    else:
+        await message.channel.send("WowWowWow!")
 
 client.run(os.environ['DISCORD_TOKEN'])
