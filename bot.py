106e17d7874bf69cee14c59ca90e503f3f48ce93
LiquidRekt <dung6ctd@gmail.com>
2020-04-25 22:09:51 +0700

    inspect fetchone()

--- bot.py
+++ bot.py
@@ -66,7 +66,8 @@ async def on_message(message):
                     await message.channel.send('Mã Deck dã được giải rồi! Yay~')
                     await message.channel.send(embed = deckCompiler(code))
         if message.content.startswith('!wallet'):
-            await message.channel.send(f"{message.author.mention} Bạn đã tạo ví mới! ID của ví bạn là: {db_getter.addUserEconomyData()}")
+            print(db_getter.addUserEconomyData())
+            #await message.channel.send(f"{message.author.mention} Bạn đã tạo ví mới! ID của ví bạn là: {db_getter.addUserEconomyData()}")
         if "admin" in [y.name.lower() for y in message.author.roles]:
             if message.content.startswith('$clear'):
                 print(str(message.author.roles))
--- db_getter.py
+++ db_getter.py
@@ -8,6 +8,7 @@ cur = conn.cursor()
 
 def addUserEconomyData():
     cur.execute("SELECT id_num FROM id_get")
-    return cur.fetchone()
+    results cur.fetchone()
+    return results
     #cur.execute("INSERT INTO economy VALUES(%s %s %s)",
     #(id, discord, currency))
