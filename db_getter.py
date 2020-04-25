bc2484e9c7973bb545fc81a632acf3be0113288f
LiquidRekt <dung6ctd@gmail.com>
2020-04-25 22:34:04 +0700

    testing !wallet command

--- db_getter.py
+++ db_getter.py
@@ -11,7 +11,7 @@ def addUserEconomyData(discord, amount):
     results = cur.fetchone()
     id = results[0]
     new_id = id + 1
-    cur.execute("UPDATE id_get SET id_num=%s",(new_id))
+    cur.execute("UPDATE id_get SET id_num=%s",(new_id,))
     cur.execute("INSERT INTO economy VALUES(%s %s %s)",
     (id, discord, amount))
     return {"id":id }
