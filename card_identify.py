eb8a6923c92ce11fbf5ccd446372f01dec45a83c
LiquidRekt <dung6ctd@gmail.com>
2020-04-27 23:18:55 +0700

    update 1.0.0

--- card_identify.py
+++ card_identify.py
@@ -1,14 +1,14 @@
 import requests
 import json
 response1 = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/en_us/data/set1-en_us.json").text)
-response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/set2-en_us.json").text)
+#response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/set2-en_us.json").text)
 
 cards_data = {}
 
 for card in response1:
     cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
 
-for card in response2:
-    cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+#for card in response2:
+#   cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
 
     
