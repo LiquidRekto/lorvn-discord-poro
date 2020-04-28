18dabe9b2554207d1af85839f8173a7b86a0cf41
LiquidRekt <dung6ctd@gmail.com>
2020-04-28 07:53:46 +0700

    add !card function

--- card_image.py
+++ card_image.py
@@ -7,7 +7,7 @@ response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/
 image_urls = {}
 
 for card in response1:
-    image_urls[card['name']] = { "CardArt": f"{card['gameAbsolutePath']}", "FullArt": f"{card['fullAbsolutePath']}" }
+    image_urls[card['name']] = { "CardArt": f"{(card['assets'])['gameAbsolutePath']}", "FullArt": f"{(card['assets'])['fullAbsolutePath']}" }
 
 for card in response2:
-   image_urls[card['name']] = { "CardArt": f"{card['gameAbsolutePath']}", "FullArt": f"{card['fullAbsolutePath']}" }
+   image_urls[card['name']] = { "CardArt": f"{(card['assets'])['gameAbsolutePath']}", "FullArt": f"{(card['assets'])['fullAbsolutePath']}" }
