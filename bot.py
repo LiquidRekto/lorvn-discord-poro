b120692a47d2d0f2e08c07d963dbebf33ae45521
LiquidRekt <dung6ctd@gmail.com>
2020-08-02 17:56:55 +0700

    update function

--- bot.py
+++ bot.py
@@ -130,8 +130,8 @@ async def on_message(message):
                         if (ctx[1:].index(question_chunk) < len(ctx[1:]) - 1):
                             question += " "
                     me = discord.utils.get(client.get_all_members(), id=670673783002103811)
-                    await message.send_message(me,f"Người dùng tên **{message.author}** đã hỏi:\n*{question}*")
-                    await me.send(f"{message.author.mention} Gửi đóng góp thành công!")
+                    await me.send_message(f"Người dùng tên **{message.author}** đã hỏi:\n*{question}*")
+                    await message.channel.send(f"{message.author.mention} Gửi đóng góp thành công!")
                 except Exception as e:
                     print(str(e))
                     await message.channel.send(f"{message.author.mention} Đóng góp thất bại! XIn hãy thử lại!")
