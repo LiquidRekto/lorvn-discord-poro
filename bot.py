cae4c1bbcda8e7ac6cb6b39efa088c288110d7e5
LiquidRekt <dung6ctd@gmail.com>
2020-08-02 21:27:04 +0700

    update shop func

--- bot.py
+++ bot.py
@@ -393,7 +393,7 @@ async def on_message(message):
                             msg = "\nTạo mặt hàng mới không thành công! \nLí do: Thiếu {}".format(missers)
                             await message.channel.send(f"{message.author.mention} %s" % msg)
                         else:
-                            result = db_getter.addShopFunction(shop_ctx[0],shop_ctx[2],shop_ctx[3],shop_ctx[1])
+                            result = db_getter.addShopFunction(shop_ctx[0],shop_ctx[1],shop_ctx[2],shop_ctx[3])
                             if (result == 'success'):
                                 await message.channel.send(f"{message.author.mention} Khởi tạo mặt hàng thành công! Đã thêm vào kệ hàng!")
                             elif (result == 'duplicated'):
