53444a0c289fb16631274e22ee9fdececc3450fb
Bùi Tiến Dũng <46742676+LiquidRekto@users.noreply.github.com>
2020-08-13 16:06:11 +0700

    Prepare for set 3

--- card_identify.py
+++ card_identify.py
@@ -2,6 +2,7 @@ import requests
 import json
 response1 = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/en_us/data/set1-en_us.json").text)
 response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/set2-en_us.json").text)
+response3 = json.loads(requests.get("http://dd.b.pvp.net/latest/set3/en_us/data/set3-en_us.json").text)
 
 cards_data = {}
 
@@ -10,5 +11,12 @@ for card in response1:
 
 for card in response2:
    cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+                                   
+try:
+    for card in response3:
+        cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+except Exception as e:
+    print(e)
+
 
     
