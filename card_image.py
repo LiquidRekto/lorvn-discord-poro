da6e4cbe72d470c17be27ba6c99110b3fb43643b
Bùi Tiến Dũng <46742676+LiquidRekto@users.noreply.github.com>
2020-08-13 16:17:29 +0700

    Prepare for set 3

--- card_image.py
+++ card_image.py
@@ -3,6 +3,7 @@ import json
 
 response1 = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/en_us/data/set1-en_us.json").text)
 response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/set2-en_us.json").text)
+response3 = json.loads(requests.get("http://dd.b.pvp.net/latest/set3/en_us/data/set3-en_us.json").text)
 
 image_urls = {}
 
@@ -25,4 +26,18 @@ for card in response2:
         image_urls[card['name']].append({"isLevelledUp": False, "CardArt": f"{source['gameAbsolutePath']}", "FullArt": f"{source['fullAbsolutePath']}" })
     else:
         image_urls[card['name']].append({ "CardArt": f"{source['gameAbsolutePath']}", "FullArt": f"{source['fullAbsolutePath']}" })
+                                         
+try:
+    for card in response3:
+    image_urls.setdefault(card['name'], [])
+    source = (card['assets'])[0]      
+    if card['levelupDescription'] == "" and card['rarity'] == "None":
+        image_urls[card['name']].append({"isLevelledUp": True, "CardArt": f"{source['gameAbsolutePath']}", "FullArt": f"{source['fullAbsolutePath']}" })
+    elif card['levelupDescription'] != "" and card['rarity'] == "Champion":
+        image_urls[card['name']].append({"isLevelledUp": False, "CardArt": f"{source['gameAbsolutePath']}", "FullArt": f"{source['fullAbsolutePath']}" })
+    else:
+        image_urls[card['name']].append({ "CardArt": f"{source['gameAbsolutePath']}", "FullArt": f"{source['fullAbsolutePath']}" })
+except Exception as e:
+    print(e)
+    
     
