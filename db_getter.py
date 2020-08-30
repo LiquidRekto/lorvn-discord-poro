cd47c2bdc8e0331c225f58aed79598a88065666a
LiquidRekt <dung6ctd@gmail.com>
2020-08-30 11:03:15 +0700

    fix steal limit functions

--- db_getter.py
+++ db_getter.py
@@ -205,9 +205,9 @@ def stealSnax(selfWallet, targetWallet, isOnline):
                         return 'unlucky'
                 else:
                     return 'no-snax'
-                new_cd = str(utilities.dateTimeAddTime(utilities.getCurrentDatetime(),0,0,1,0))
-                cur.execute("UPDATE economy SET steal_cd = %s WHERE discord_id = '%s'" % (new_cd, targetWallet))
-                cur.execute("UPDATE economy SET steal_count = %s WHERE discord_id = '%s'" % (stealCount+1, targetWallet))
+                new_cd = utilities.dateTimeAddTime(utilities.getCurrentDatetime(),0,0,1,0)
+                cur.execute("UPDATE economy SET steal_cd = %s WHERE discord_id = '%s'" % (new_cd, selfWallet))
+                cur.execute("UPDATE economy SET steal_count = %s WHERE discord_id = '%s'" % (stealCount+1, selfWallet))
                 conn.commit()
         else:
             return 'limit-reached'
