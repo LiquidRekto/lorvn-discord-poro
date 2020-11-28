788c6db6074d4f9445799967bfda8ee95704d2ed
Trollerr <42436350+trollerr@users.noreply.github.com>
2020-11-28 20:59:37 +0700

    add ','

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ def regionEmote(reg, cli):
         'Noxus':'704619523239706655',
         'Ionia':'704619522706898996',
         'PiltoverZaun':'704619522715418737',
-        'ShadowIsles':'704619523172597860'
+        'ShadowIsles':'704619523172597860',
         'Targon':'782238199845093376'
     }
     defined = False
