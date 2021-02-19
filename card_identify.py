835391b93c07b398983ad2a64b0a260679bb718f
Trollerr <42436350+trollerr@users.noreply.github.com>
2021-02-19 19:53:00 +0700

    add English name and set 4 cards

--- card_identify.py
+++ card_identify.py
@@ -2,8 +2,13 @@ import requests
 import json
 response1 = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/vi_vn/data/set1-vi_vn.json").text)
 response2 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/vi_vn/data/set2-vi_vn.json").text)
+response3 = json.loads(requests.get("http://dd.b.pvp.net/latest/set3/vi_vn/data/set3-vi_vn.json").text)
+response4 = json.loads(requests.get("http://dd.b.pvp.net/latest/set4/vi_vn/data/set4-vi_vn.json").text)
 try:
-    response3 = json.loads(requests.get("http://dd.b.pvp.net/latest/set3/vi_vn/data/set3-vi_vn.json").text)
+    response5 = json.loads(requests.get("http://dd.b.pvp.net/latest/set1/en_us/data/set1-en_us.json").text)
+    response6 = json.loads(requests.get("http://dd.b.pvp.net/latest/set2/en_us/data/set2-en_us.json").text)
+    response7 = json.loads(requests.get("http://dd.b.pvp.net/latest/set3/en_us/data/set3-en_us.json").text)
+    response8 = json.loads(requests.get("http://dd.b.pvp.net/latest/set4/en_us/data/set4-en_us.json").text)
 except Exception as e:
     print(e)
 
@@ -15,8 +20,23 @@ for card in response1:
 for card in response2:
    cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
 
+for card in response3:
+   cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+                                   
+for card in response4:
+   cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+
 try:
-    for card in response3:
+    for card in response5:
+        cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+                                        
+    for card in response6:
+        cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+                                        
+    for card in response7:
+        cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
+                                        
+    for card in response8:
         cards_data[card['cardCode']] = { "Name": f"{card['name']}"}
 except Exception as e:
     print(e)
